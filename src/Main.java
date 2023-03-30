8d7a5fd5780bc9694db5129a8eb98070b136d549
AnnNetoTest <yaskovaann2002@mail.ru>
2023-03-30 16:57:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int oneBonus = 100;
+        int summPlus = 1690;
+        int bonus;
+        boolean more1000 = true;
+
+        if (more1000) {
+            bonus = summPlus / oneBonus;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+    }
+}
+
+
+
+
+
